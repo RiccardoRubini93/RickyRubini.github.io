219bdf04d9dd013f975c2fa0abe76971c643a21c
RiccardoRubini93 <riru93@gmail.com>
2017-06-15 09:48:30 +0200

    mobile correction

--- js/demo-1.js
+++ js/demo-1.js
@@ -1,3 +1,13 @@
+function detectmobile() {
+   if(window.innerWidth <= 800 && window.innerHeight <= 600) {
+     return true;
+   } else {
+     return false;
+   }
+}
+
+if(!detectmobile()){
+
 (function() 
 	{
 
@@ -199,5 +209,5 @@
     }
     
 })();
-
+}
 
